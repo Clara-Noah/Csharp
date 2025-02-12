f1a3c308bc08e786197564f27ee3f59cc681fb20
Clara-Noah <claraclaudio806@gmail.com>
2025-02-12 10:49:17 +0000

    Add files via upload

---
+++ exercicio17/exercicio17/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+public class Exercicio17 {
+
+    public static void Main(string[] args)
+    {
+        //String é usada para nomes ou letras numa variavel enquanto int é apenas para numeros
+        String[] listafilmes = { "harry potter", "zoro", "simpsons" };
+        int[] ano = { 1999, 2010, 2014 };
+
+        Console.WriteLine("Lista de filmes: ");
+
+        //foreach quando usado salta de linha automaticamente enquanto o for não
+        for (int contador = 0; contador < listafilmes.Length; contador++ )
+        {
+            Console.WriteLine(listafilmes[contador]+ " - " + ano[contador]);
+            }
+    }
+}
---
+++ exercicio18/exercicio18/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Exercicio18
+{
+
+    public static void Main(string[] args)
+    {
+        //String é usada para nomes ou letras numa variavel enquanto int é apenas para numeros inteiros, sempre quando usado nomes em String tem que ser entre " "
+        String[] cidades = { "lisboa", "porto", "coimbra", "faro", "braga" };
+        int[] habitantes = { 545000, 237000, 143000, 118000, 193000 };
+
+        Console.WriteLine("Cidades e sua população: ");
+
+        //foreach quando usado salta de linha automaticamente enquanto o for não, podemos usar + " " + para adicionar algo entre os arrays
+        for (int contador = 0; contador < cidades.Length; contador++)
+        {
+            Console.WriteLine(cidades[contador] + " - " + habitantes[contador] + " habitantes");
+        }
+    }
+}
+
+// feito por Aimar e Clara
