ad19b8ea214fc5904113b5141fccf603647895f5
Clara-Noah <claraclaudio806@gmail.com>
2025-03-12 09:42:12 +0000

    Update Program.cs

--- Exercicio_19/Exercicio_19/Program.cs
+++ Exercicio_19/Exercicio_19/Program.cs
@@ -20,22 +20,20 @@ public class exercicio19
         Console.WriteLine("Escolha uma opção: ");
         //variavel opcao
         String opcao = Console.ReadLine();
-        do
-        {
+     
             switch (opcao)
             {
                 case "1":
                     Console.WriteLine("Introduza o nome do contacto: ");
                     String nome = Console.ReadLine();
                     Console.WriteLine("Introduza o número do contacto: ");
-                    String Contacto = Console.ReadLine();
-                    listatelefonica.Add(nome, Contacto);
+                    String contacto = Console.ReadLine();
+                    listatelefonica.Add(nome, contacto);
                     Console.WriteLine("Contacto adicionado com sucesso");
                     break;
 
                 case "2":
-                    //Adicionar contacto e numero para que o programa possa procurar
-                    listatelefonica.Add("alice", "123456789");
+
                     Console.WriteLine("Introduza o nome do contacto que deseja procurar: ");
 
                     //nomepesquisa é a variavel que o utilizador vai introduzir para procurar p contacto
@@ -51,8 +49,7 @@ public class exercicio19
                     }
                     break;
                 case "3":
-                    //Adicionar contacto e numero para que o programa possa remove-lo
-                    listatelefonica.Add("alice", "123456789");
+                  
                     Console.WriteLine("Introduza o contacto que deseja remover: ");
                     nomepesquisa = Console.ReadLine();
                     if (listatelefonica.ContainsKey(nomepesquisa))
@@ -65,13 +62,16 @@ public class exercicio19
                         Console.WriteLine("O contacto " + nomepesquisa + " não existe");
                     }
                     break;
-                default:
+                case "4":
                     //caso o utilizador escolha a opção 4, ou seja, nenhuma das 3 opções anteriores, o programa vai sair
                     Console.WriteLine("saindo. . . . . ");
                     break;
+
+                    //se nenhuma das 4 opções for escolhida, esta opção será acionada
+                default:
+                    Console.WriteLine("opção invalida")
+                        break;
             }
-        }
-        //enquanto a opção for diferente, menos de 4, o programa vai continuar a correr
-        while (opcao != "4");
+        
     }
 }
